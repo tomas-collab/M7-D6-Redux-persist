7ec8e150815940b9493f0b440225b06252a9967e
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-09-21 08:15:05 +0200

    redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import favoriteReducer from "../reducer/favorite";
 import thunk from "redux-thunk";
 import jobsReducer from "../reducer/jobs";
+import storage from 'redux-persist/lib/storage' 
+import { persistStore,persistReducer } from "redux-persist";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 
 export const initialState = {
@@ -15,15 +17,20 @@ export const initialState = {
     },
 }
 
+const persistConfig = {
+    key:'root',
+    storage
+}
 const bigReducer = combineReducers({
     favorite:favoriteReducer,
     jobs:jobsReducer
 })
 
-const configureStore = createStore(
-    bigReducer,
+const persistReducer = persistReducer(persistConfig,bigReducer)
+export const configureStore = createStore(
+    persistReducer,
     initialState,
     process.env.REACT_APP_DEVELOPMENT? composeEnhancers(applyMiddleware(thunk)):compose(applyMiddleware(thunk))
   )
 
-  export default configureStore
+ export const persistor = persistStore(configureStore)
